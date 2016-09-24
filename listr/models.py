c7fd68fb634449badc495e1c2ed63606c04e7076
kdenny <kdenny@deloitte.com>
2016-09-24 13:06:30 -0400

    Changes to places view

--- listr/models.py
+++ listr/models.py
@@ -39,6 +39,8 @@ class List(models.Model):
 
     places = models.ManyToManyField(Place)
 
+    # collaborators = models.ManyToManyField(User)
+
     def __unicode__(self):              # __unicode__ on Python 2
         return str(self.id)
 
