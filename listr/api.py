e05867246e80e3be4a4d354030d7770141440dc3
kdenny <kdenny@deloitte.com>
2016-11-21 22:54:21 -0500

    Changes to places view

--- listr/api.py
+++ listr/api.py
@@ -11,6 +11,18 @@ class ListResource(ModelResource):
         queryset = List.objects.all()
         resource_name = 'list'
 
+# class RemoveFromList(ModelResource):
+#
+#     def obj_delete(self, bundle, **kwargs):
+#          # get post id
+#          list = List.objects.get(pk=bundle.data.id) # or or whatever way you can get the id
+#          # delete all comments with that post id
+#          Comment.objects.filter(post=comment.post).delete()
+#          return super(RemoveFromList, self).obj_delete(bundle, user=bundle.request.user)
+#
+#     def apply_authorization_limits(self, request, object_list):
+#         return object_list.filter(user=request.user)
+
 
 class PlaceResource(ModelResource):
     list = fields.ToOneField(ListResource, 'list')
